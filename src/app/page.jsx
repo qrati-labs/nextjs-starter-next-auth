0287bc16ecf1607bf45a2baff6b9b8112c51a074
Siddhartha Maity <smaity@qrati.com>
2023-11-19 01:02:30 +0530

    Hydration mismatch fixed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import ThemeSwitcher from 'src/components/ThemeSwitcher';
 
 const Home = () => {
     return (
-        <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+        <main className='flex min-h-screen flex-col items-center justify-between pb-0 pt-24 lg:pb-24'>
             <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
                 <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
                     Get started by editing&nbsp;
@@ -39,14 +39,16 @@ const Home = () => {
                     priority
                 />
             </div>
-            <div>
+            <div className='my-4 max-w-lg text-center lg:my-0'>
                 PNPM • EditorConfig • VSCode Configs & Extensions • ESLint • Prettier • Sort Imports • Tailwind CSS •
                 Next Themes • Bundle Analyzer • Dockerfile
             </div>
 
-            <ThemeSwitcher />
+            <div className='my-4 lg:my-0'>
+                <ThemeSwitcher />
+            </div>
 
-            <div className='mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left'>
+            <div className='mb-32 grid grid-cols-2 text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left'>
                 <a
                     href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
                     className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
